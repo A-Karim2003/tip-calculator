fc6360af52310e8f7516dc2d2028d30c4cfb0936
Abdoulkarim <17a.karim@gmail.com>
2025-08-02 21:48:39 +0100

    refactor: convert static tip inputs to dynamic controlled components
    - Introduced state hooks for bill, user service rating, and friend’s rating
    - Refactored duplicate service dropdowns into a reusable <Service /> component
    - Implemented a shared handleService function to update state dynamically

--- src/components/TipCalculator.jsx
+++ src/components/TipCalculator.jsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+
 function TipCalculator() {
+  const [bill, setBill] = useState(0);
+  const [service, setService] = useState("");
+  const [friendService, setfriendService] = useState("");
+
+  function handleBill(e) {
+    setBill(Math.abs(Number(e.target.value)));
+    console.log(bill);
+  }
+
+  function handleService(e, setState) {
+    setState(Number(e.target.value));
+  }
+
   return (
     <div className="container">
       <h1 className="title">✨ Tip Calculator</h1>
@@ -6,40 +21,52 @@ function TipCalculator() {
       <div className="form-group">
         <label htmlFor="bill">How much was the bill?</label>
 
-        <input type="number" id="bill" placeholder="Enter bill amount" />
+        <input
+          type="number"
+          id="bill"
+          placeholder="Enter bill amount"
+          onChange={handleBill}
+        />
       </div>
-      <div className="form-group">
-        <label htmlFor="service"> How did you like the service? </label>
-        <select id="service">
-          <option value="" selected>
-            Select your rating
-          </option>
-          <option value="0.10">🤔 It was okay (10%)</option>
-          <option value="0.15">😊 Good service (15%)</option>
-          <option value="0.18">😍 Great service (18%)</option>
-          <option value="0.20">🤩 Excellent service (20%)</option>
-          <option value="0.25">🔥 Outstanding! (25%)</option>
-        </select>
-      </div>
-      <div class="form-group">
-        <label for="friendService">How did your friend like the service?</label>
-        <select id="friendService">
-          <option value="" selected>
-            Select friend's rating
-          </option>
-          <option value="0.10">🤔 They thought it was okay (10%)</option>
-          <option value="0.15">😊 They liked it (15%)</option>
-          <option value="0.18">😍 They loved it (18%)</option>
-          <option value="0.20">🤩 They were impressed (20%)</option>
-          <option value="0.25">🔥 They were blown away! (25%)</option>
-        </select>
-      </div>
+
+      <Service
+        service={service}
+        onHandleService={(e) => handleService(e, setService)}
+        placeholder="Select you rating"
+      >
+        How did you like the service?
+      </Service>
+
+      <Service
+        service={friendService}
+        onHandleService={(e) => handleService(e, setfriendService)}
+        placeholder="Select your friend's rating"
+      >
+        How did your friend like the service?
+      </Service>
+
       <div className="result">
-        <h3>You pay $xxx</h3>
+        <h3>You pay ${bill}</h3>
         <p>($20 + $12 tip)</p>
       </div>
 
       <button className="result-btn">Reset Calculator</button>
+    </div>
+  );
+}
+
+function Service({ children, placeholder, onHandleService }) {
+  return (
+    <div className="form-group">
+      <label htmlFor="service"> {children} </label>
+      <select id="service" onChange={onHandleService}>
+        <option value="">{placeholder}</option>
+        <option value="10">🤔 It was okay (10%)</option>
+        <option value="15">😊 Good service (15%)</option>
+        <option value="18">😍 Great service (18%)</option>
+        <option value="20">🤩 Excellent service (20%)</option>
+        <option value="25">🔥 Outstanding! (25%)</option>
+      </select>
     </div>
   );
 }
